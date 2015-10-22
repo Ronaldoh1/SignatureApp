0a9767a24c01e38735408a9451a7be2f88813a47
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-10-22 10:25:51 -0700

    Initial Commit

---
+++ SignatureApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SignatureApp
+//
+//  Created by Ronald Hernandez on 10/22/15.
+//  Copyright © 2015 Wahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
